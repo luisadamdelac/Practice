230ce32c7a497a8d66a14c779c9af346f9ffccb0
Dela Cruz, Luis Adam D <luisadamdelacruz8@gmail.com>
2024-05-31 10:00:42 +0800

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,11 @@
+
+            function jokelang() {
+                document.getElementById("callme").innerHTML="Joke Lang!">
+                document.getElementById("")
+            }
+            function changecolor(){
+                document.getElementById("callme").style.color="blue">
+            }
+            
+        
+    
